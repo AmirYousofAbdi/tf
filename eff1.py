a317c31ff7c81cdfe7809e91adac26a8e422944d
AmirYousofAbdi <56346544+AmirYousofAbdi@users.noreply.github.com>
2020-05-13 11:48:16 +0430

    Update eff1.py

--- eff1.py
+++ eff1.py
@@ -9,7 +9,7 @@ from keras import optimizers
 import matplotlib.pyplot as plt
 ###############   defineing fucntions
 def pdata():
-    dataset = pd.read_csv('C:\\Users\\Lenovo\\Desktop\\Programming\\Sani\\final\\data.txt',names = ['Date','Open','High','Low','Close','Volume','OpenInt'])
+    dataset = pd.read_csv('.csv file path',names = ['Date','Open','High','Low','Close','Volume','OpenInt'])
     dataset = dataset.drop('Date',axis = 1)
     dataset = dataset.drop('OpenInt',axis = 1)
     dataset = dataset.drop(0,axis = 0)
@@ -18,7 +18,7 @@ def pdata():
     data_normalised = data_normaliser.fit_transform(dataset)
     global history_points
     history_points = 14
-    ohlcv_histories_normalised =      np.array([data_normalised[i  : i + history_points].copy() for i in range(len(data_normalised) - history_points)])
+    ohlcv_histories_normalised =  np.array([data_normalised[i  : i + history_points].copy() for i in range(len(data_normalised) - history_points)])
     next_day_open_values_normalised = np.array([data_normalised[i + history_points][0].copy() for i in range(len(data_normalised) - history_points)])
     next_day_open_values_normalised = np.expand_dims(next_day_open_values_normalised, -1)
     next_day_open_values = np.array([data['Open'][i + history_points] for i in range(len(data) - history_points)])
@@ -32,15 +32,14 @@ def mse(real,predict):
     for i in range(length):
         msecalc += (real[i] - predict[i]) ** 2
     return msecalc / length
-def dtm(ls):
+def n2l(ls):
     ls2 = []
     for i in ls:
         ls2.append(float(i[0]))
     return ls2
 ###############   preaparing normalised data to be traind
 ohlcv_histories, next_day_open_values, unscaled_y, y_normaliser = pdata()
-test_split = 0.9
-n = int(ohlcv_histories.shape[0] * test_split)
+n = int(ohlcv_histories.shape[0] * 0.9)
 ohlcv_train = ohlcv_histories[:n]
 y_train = next_day_open_values[:n]
 ohlcv_test = ohlcv_histories[n:]
@@ -58,8 +57,8 @@ model.fit(x=ohlcv_train, y=y_train, batch_size=64, epochs=50)
 y_test_predicted = model.predict(ohlcv_test)
 y_test_predicted = y_normaliser.inverse_transform(y_test_predicted)
 ###############   plotting the results
-unscaled_y_test = dtm(unscaled_y_test.tolist())
-y_test_predicted = dtm(y_test_predicted.tolist())
+unscaled_y_test = n2l(unscaled_y_test.tolist())
+y_test_predicted = n2l(y_test_predicted.tolist())
 print(str("{:.2f}".format(mse(unscaled_y_test,y_test_predicted)*100))+'% Mistake in prediction')
 plt.plot(unscaled_y_test, label='real',color = 'g')
 plt.plot(y_test_predicted, label='predicted', color = 'r')
